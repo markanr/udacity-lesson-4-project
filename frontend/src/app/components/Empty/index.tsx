ef4ef29e1099c53dbc8b3b44217b920fadbaecf5
Marko <marko.radanovic@outlook.com>
2021-04-26 22:12:41 +0200

    fixing audit error and frontent unit tests

--- frontend/src/app/components/Empty/index.tsx
+++ frontend/src/app/components/Empty/index.tsx
@@ -12,10 +12,10 @@ export const Empty = ({ title }: Emtpy.Props) => {
     <div className={style['no-orders']}>
       <span className={style.icon} />
       <h3 className={style.title}>{title}</h3>
-      <span>If you need more information please contact us:</span>
+      {/* <span>If you need more information please contact us:</span>
       <a href={`mailto:${process.env.WP_HELP_EMAIL}`} className={style.email}>
         {process.env.WP_HELP_EMAIL}
-      </a>
+      </a> */}
     </div>
   );
 };
